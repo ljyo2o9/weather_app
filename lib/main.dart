c8f6d77768ba25e1411f64694033c715d8821ec8
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-05 22:50:32 +0900

    main.dart flutter_screennutil 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:weather_app/ui/view/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +12,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      builder: (context, child) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+        );
+      },
+    );
   }
 }
