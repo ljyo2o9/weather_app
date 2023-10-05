4dd37f5b3fbc023d482c537f44a16e0c8e764a38
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-06 08:10:19 +0900

    weather_repository 생성

---
+++ lib/data/repository/weather_repository.dart
@@ -0,0 +1,10 @@
+import 'package:weather_app/data/model/weather_model.dart';
+import 'package:weather_app/data/dataSource/weather_data.dart';
+
+class WeatherRepository {
+  final WeatherData _weatherData = WeatherData();
+
+  Future<List<WeatherModel>> getWeatherList() async {
+    return _weatherData.getWeatherList();
+  }
+}
