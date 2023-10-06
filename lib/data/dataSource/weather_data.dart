91208380d9101b753177c5f3bc925cb74c4e81ca
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-06 14:37:27 +0900

    weather_data 수정

--- lib/data/dataSource/weather_data.dart
+++ lib/data/dataSource/weather_data.dart
@@ -3,12 +3,12 @@ import 'package:weather_app/data/model/weather_model.dart';
 import 'package:weather_app/ignore.dart';
 
 class WeatherData {
-  Future<List<WeatherModel>> getWeatherList() async {
+  Future<List<Item>> getWeatherList() async {
     Dio dio = Dio();
 
     final response = await dio.get(url);
-    return response.data
-        .map<WeatherModel>((json) => WeatherModel.fromJson(json))
-        .toList();
+    var weatherData = WeatherModel.fromJson(response.data);
+
+    return weatherData.response?.body?.items?.item ?? [];
   }
 }
