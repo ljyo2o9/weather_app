13f0e07c3147267c3a61dc412762806098e07a96
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-09 17:59:37 +0900

    week_data 생성

---
+++ lib/data/dataSource/week_data.dart
@@ -0,0 +1,30 @@
+class WeekData {
+  Future<String> getWeekData() async {
+    String dayOfWeek = _getDayOfWeek(DateTime
+        .now()
+        .weekday);
+
+    return dayOfWeek;
+  }
+
+  String _getDayOfWeek(int weekday) {
+    switch (weekday) {
+      case DateTime.monday:
+        return 'Monday';
+      case DateTime.tuesday:
+        return 'Tuesday';
+      case DateTime.wednesday:
+        return 'Wednesday';
+      case DateTime.thursday:
+        return 'Thursday';
+      case DateTime.friday:
+        return 'Friday';
+      case DateTime.saturday:
+        return 'Saturday';
+      case DateTime.sunday:
+        return 'Sunday';
+      default:
+        return '';
+    }
+  }
+}
