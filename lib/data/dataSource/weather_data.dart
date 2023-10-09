29e9984b41c721ba2f432ea623707eab390ce70f
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-09 22:52:32 +0900

    weather_data 수정
    Url 수정

--- lib/data/dataSource/weather_data.dart
+++ lib/data/dataSource/weather_data.dart
@@ -5,11 +5,9 @@ import 'package:weather_app/ignore.dart';
 class WeatherData {
   Future<List<Item>> getWeatherList() async {
     Dio dio = Dio();
-
-    String hour = nowHour();
-
+    
     final response = await dio.get(
-      '$url$serviceKey$pageSetting&base_date=$date&base_time=$hour$xy',
+      '$url$serviceKey$pageSetting&base_date=$date&base_time=$nowHour$xy',
     );
     var weatherData = WeatherModel.fromJson(response.data);
 
