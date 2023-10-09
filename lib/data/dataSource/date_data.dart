1cbf35b39339195acf8f6bd51181445e2c58a29b
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-09 18:28:13 +0900

    month_data 생성

--- lib/data/dataSource/date_data.dart
+++ lib/data/dataSource/date_data.dart
@@ -26,3 +26,42 @@ class WeekData {
     }
   }
 }
+
+class MonthData {
+  Future<String> getMonthData() async {
+    String month = await _getMonth(DateTime.now().month);
+
+    return month;
+  }
+
+  Future<String> _getMonth(int monthData) async {
+    switch (monthData) {
+      case 1:
+        return 'January';
+      case 2:
+        return 'February';
+      case 3:
+        return 'March';
+      case 4:
+        return 'April';
+      case 5:
+        return 'May';
+      case 6:
+        return 'June';
+      case 7:
+        return 'July';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'October';
+      case 11:
+        return 'November';
+      case 12:
+        return 'December';
+      default:
+        return '';
+    }
+  }
+}
