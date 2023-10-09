e2eb4c20ba53bbe628cc51abe05c6faff8c49e0b
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-09 22:53:21 +0900

    home_page 수정
    View는 바뀐 부분이 없고 받는 데이터가 달라짐

--- lib/ui/view/home_page.dart
+++ lib/ui/view/home_page.dart
@@ -19,118 +19,126 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     /// weekViewModel
     var dateViewModel = Provider.of<DateViewModel>(context);
-    //
-    /// 요일
-    String dayOfWeek = dateViewModel.dayOfWeek;
-    //
-    /// 일
-    var day = DateTime.now().day >= 10
-        ? DateTime.now().day
-        : '0${DateTime.now().day}';
-    //
-    /// 월
-    String month = dateViewModel.monthData;
 
     /// weatherViewModel
     var weatherViewModel = Provider.of<WeatherViewModel>(context);
-    //
-    /// 온도
-    final tmp = weatherViewModel.category['tmp'];
-    //
-    /// 풍속
-    final wsd = weatherViewModel.category['wsd'];
-    //
-    ///강수 확률
-    final pop = weatherViewModel.category['pop'];
 
-    return Scaffold(
-      body: weatherViewModel.weatherList.isNotEmpty
-          ? Padding(
-              padding: EdgeInsets.only(left: 31.w, right: 31.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  /// Divider
-                  Padding(
-                    padding: EdgeInsets.only(top: 132.h),
-                    child: Container(
-                      color: Colors.black,
-                      width: double.infinity,
-                      height: 1.h,
-                    ),
-                  ), //
-                  /// 지역
-                  Padding(
-                    padding: EdgeInsets.only(top: 40.h),
-                    child: Text(
-                      '유성구',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24.3.sp,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ), //
-                  /// 날짜
-                  Padding(
-                    padding: EdgeInsets.only(top: 17.h),
-                    child: Text(
-                      '$dayOfWeek - $day $month',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24.3.sp,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
+    if (weatherViewModel.weatherList.isNotEmpty) {
+      //
+      /// 요일
+      String dayOfWeek = dateViewModel.dayOfWeek;
+      //
+      /// 일
+      var day = DateTime.now().day >= 10
+          ? DateTime.now().day
+          : '0${DateTime.now().day}';
+      //
+      /// 월
+      String month = dateViewModel.monthData;
+
+      /// 온도
+      final tmp =
+          double.parse(weatherViewModel.weatherList[3].obsrValue.toString())
+              .toStringAsFixed(0);
+      //
+      /// 풍속
+      final wsd = weatherViewModel.weatherList[7].obsrValue;
+      //
+      ///강수 확률
+      final pop = weatherViewModel.weatherList[0].obsrValue;
+      return Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(left: 31.w, right: 31.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Divider
+              Padding(
+                padding: EdgeInsets.only(top: 132.h),
+                child: Container(
+                  color: Colors.black,
+                  width: double.infinity,
+                  height: 1.h,
+                ),
+              ), //
+              /// 지역
+              Padding(
+                padding: EdgeInsets.only(top: 40.h),
+                child: Text(
+                  '유성구',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24.3.sp,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w700,
                   ),
-                  //
-                  /// 온도
-                  int.parse(tmp) < 0
-                      ? Padding(
-                          padding: EdgeInsets.only(top: 31.h, bottom: 35.h),
-                          child: Text(
-                            '$tmp°',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 165.sp,
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
-                        )
-                      : Padding(
-                          padding: EdgeInsets.only(top: 25.h),
-                          child: Text(
-                            '$tmp°',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 200.sp,
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
-                        ),
-                  //
-                  /// 날씨
-                  Text(
-                    'It\'s sun time',
-                    style: TextStyle(
-                      fontSize: 24.sp,
-                      fontFamily: 'Roboto',
-                      fontWeight: FontWeight.w900,
-                      color: Colors.black,
-                    ),
+                ),
+              ), //
+              /// 날짜
+              Padding(
+                padding: EdgeInsets.only(top: 17.h),
+                child: Text(
+                  '$dayOfWeek - $day $month',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24.3.sp,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 48.h),
-                    child: rowCategory('Wind: ${wsd}m/s', 'Probability: $pop%'),
-                  ),
-                ],
+                ),
               ),
-            )
-          : const Center(child: CircularProgressIndicator()),
-    );
+              //
+              /// 온도
+              int.parse(tmp) < 0
+                  ? Padding(
+                      padding: EdgeInsets.only(top: 31.h, bottom: 35.h),
+                      child: Text(
+                        '$tmp°',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 165.sp,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    )
+                  : Padding(
+                      padding: EdgeInsets.only(top: 25.h),
+                      child: Text(
+                        '$tmp°',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 200.sp,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+
+              /// 날씨
+              Text(
+                'It\'s sun time',
+                style: TextStyle(
+                  fontSize: 24.sp,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black,
+                ),
+              ),
+              //
+              /// RowCategory
+              Padding(
+                padding: EdgeInsets.only(top: 48.h),
+                child: rowCategory('Wind: ${wsd}m/s', 'Probability: $pop%'),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
   }
 }
