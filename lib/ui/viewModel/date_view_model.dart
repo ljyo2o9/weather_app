5cd58e24710918fdff084556e2338f7973585945
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-09 18:38:52 +0900

    monthdata_view_model 생성

--- lib/ui/viewModel/date_view_model.dart
+++ lib/ui/viewModel/date_view_model.dart
@@ -3,17 +3,27 @@ import 'package:weather_app/data/repository/date_repository.dart';
 
 class DateViewModel extends ChangeNotifier {
   final WeekRepository _weekRepository = WeekRepository();
+  final MonthRepository _monthRepository = MonthRepository();
 
   String _dayOfWeek = '';
+  String _monthData = '';
 
   String get dayOfWeek => _dayOfWeek;
 
+  String get monthData => _monthData;
+
   DateViewModel() {
     _getDayOfWeek();
+    _getMonthData();
   }
 
   Future<void> _getDayOfWeek() async {
     _dayOfWeek = await _weekRepository.getDayOfWeek();
+    notifyListeners();
+  }
+
+  Future<void> _getMonthData() async {
+    _monthData = await _monthRepository.getMonthData();
     notifyListeners();
   }
 }
