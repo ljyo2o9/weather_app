fbd797a88b714e8af95e54677366a8da2c264ca5
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-09 23:27:28 +0900

    weather_view_model 수정
    comment 함수 생성과 날씨에 따른 값 변화

--- lib/ui/viewModel/weather_view_model.dart
+++ lib/ui/viewModel/weather_view_model.dart
@@ -2,21 +2,41 @@ import 'package:flutter/widgets.dart';
 
 import 'package:weather_app/data/model/weather_model.dart';
 import 'package:weather_app/data/repository/weather_repository.dart';
-import 'package:weather_app/ignore.dart';
 
 class WeatherViewModel extends ChangeNotifier {
   final WeatherRepository _weatherRepository = WeatherRepository();
-
-  List<Item> _weatherList = [];
-  
-  List<Item> get weatherList => _weatherList;
 
   WeatherViewModel() {
     _getWeatherList();
   }
 
+  String _comment = '';
+  List<Item> _weatherList = [];
+
+  String get comment => _comment;
+
+  List<Item> get weatherList => _weatherList;
+
+  Future<String> _getWeather() async {
+    if (weatherList.isEmpty) await _getWeatherList();
+    switch (_weatherList[0].obsrValue) {
+      case '0':
+        return 'It\'s sun time';
+      case '1':
+      case '4':
+      case '5':
+        return 'Yes. It\'s raining';
+      case '3':
+      case '7':
+        return 'It\'s snowing';
+      default:
+        return '';
+    }
+  }
+
   Future<void> _getWeatherList() async {
     _weatherList = await _weatherRepository.getWeatherList();
+    _comment = await _getWeather();
     notifyListeners();
   }
 }
