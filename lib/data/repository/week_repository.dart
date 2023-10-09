17d8e40c6af2fa9e769e4edb43a30c39959d2741
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-09 18:04:44 +0900

    repository 수정

--- lib/data/repository/week_repository.dart
+++ lib/data/repository/week_repository.dart
@@ -3,7 +3,7 @@ import 'package:weather_app/data/dataSource/week_data.dart';
 class WeekRepository {
   final WeekData _weekData = WeekData();
 
-  Future<String> getWeekOfDay() async {
+  Future<String> getDayOfWeeky() async {
     return _weekData.getWeekData();
   }
 }
