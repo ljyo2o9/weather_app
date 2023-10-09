e526ab6ecbf13d8a68cd74a499929d5ffbc31b59
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-09 22:52:56 +0900

    weather_view_model 수정
    더이상 데이터를 찾는 짓을 안함

--- lib/ui/viewModel/weather_view_model.dart
+++ lib/ui/viewModel/weather_view_model.dart
@@ -7,11 +7,8 @@ import 'package:weather_app/ignore.dart';
 class WeatherViewModel extends ChangeNotifier {
   final WeatherRepository _weatherRepository = WeatherRepository();
 
-  Map<String, dynamic> _category = {};
   List<Item> _weatherList = [];
-
-  Map<String, dynamic> get category => _category;
-
+  
   List<Item> get weatherList => _weatherList;
 
   WeatherViewModel() {
@@ -20,27 +17,6 @@ class WeatherViewModel extends ChangeNotifier {
 
   Future<void> _getWeatherList() async {
     _weatherList = await _weatherRepository.getWeatherList();
-
-    for (int i = 0; i < _weatherList.length; i++) {
-      if (_weatherList[i].fcstTime == ('${hourModel}00')) {
-        if (_weatherList[i].category == "TMP") {
-          _category = {
-            'tmp': _weatherList[i].fcstValue,
-            'uuu': _weatherList[i + 1].fcstValue,
-            'vvv': _weatherList[i + 2].fcstValue,
-            'vec': _weatherList[i + 3].fcstValue,
-            'wsd': _weatherList[i + 4].fcstValue,
-            'sky': _weatherList[i + 5].fcstValue,
-            'pty': _weatherList[i + 6].fcstValue,
-            'pop': _weatherList[i + 7].fcstValue,
-            'wav': _weatherList[i + 8].fcstValue,
-            'pcp': _weatherList[i + 9].fcstValue,
-            'reh': _weatherList[i + 10].fcstValue,
-            'sno': _weatherList[i + 11].fcstValue,
-          };
-        }
-      }
-    }
     notifyListeners();
   }
 }
