bcf43ed6befcbea67753e1710e1f01d00d136818
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-10 22:17:58 +0900

    function 추가
    새로고침 버튼에 쓰인 Navigation Animation

---
+++ lib/ui/function/refrash_route_animation.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/ui/view/home_page.dart';
+
+Route refrashRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const HomePage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 0.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
